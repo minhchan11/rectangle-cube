960f7bb74422e5226d230ad8e63191d977372f02
Minh Phuong and Renee Mei <minh+renee@>
2017-02-08 08:52:31 -0800

    before changing post method to get method, remove post action in form button

--- Modules/HomeModules.cs
+++ Modules/HomeModules.cs
@@ -1,5 +1,6 @@
 using Nancy;
 using Rectangles.Objects;
+using System;
 using System.Collections.Generic;
 
 namespace RectangleChecker
@@ -16,9 +17,17 @@ namespace RectangleChecker
         return View["Rectangle_all.cshtml", allRectangles];
       };
       Post["/rectangle-result"] = _ => {
+        Dictionary<string, object> Shapes = new Dictionary<string, object>();
         Rectangle inputRectangle = new Rectangle(Request.Form["length"],Request.Form["width"]);
+        Shapes.Add("ResultingRectangle", inputRectangle);
+
+        if (inputRectangle.IsSquare())
+        {
+          Cube inputCube = new Cube(inputRectangle);
+          Shapes.Add("ResultingCube", inputCube);
+        }
         inputRectangle.SaveRectangle();
-        return View["Rectangle_results.cshtml",inputRectangle];
+        return View["Rectangle_results.cshtml", Shapes];
       };
       Post["/rectangle_cleared"] = _ => {
         Rectangle.ClearAll();
